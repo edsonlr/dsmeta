5400dd2c544a1c221dce78f48b745a1ee718dd07
edsonlr <elribeirom10@gmail.com>
2022-07-13 16:24:42 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Ola Mundo!</h1>
+    <>
+      <h1>Ola Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
