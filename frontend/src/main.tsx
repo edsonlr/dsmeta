f2e2cbe226064ed46942057de9caab8cc0755a98
edsonlr <elribeirom10@gmail.com>
2022-07-17 14:49:53 -0300

    Correcao tela preta no Netlify apos envio de SMS-metodo 2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { render } from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-window.React = React
+//window.React = React
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
